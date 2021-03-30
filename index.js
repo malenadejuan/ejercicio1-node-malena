993f0c02161f6937efe7f38dd18e87e42b850820
Malena de Juan <malena.dj4@gmail.com>
2021-03-30 18:20:18 +0200

    eliminar guiones del argumento pasado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const saluda = require("./utilidades/archivos.js");
 const chalk = require("chalk");
 
 if (process.argv[2]) {
-  saluda(process.argv[2].replace("nombre=", ""));
+  saluda(process.argv[2].replace("--nombre=", ""));
 } else {
   console.log(chalk.yellow("No se ha escrito ningún parámetro."));
   process.exit(0);
