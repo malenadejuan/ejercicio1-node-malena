c876daea5eb85a016d664d9b66dac408e9cece21
Malena de Juan <malena.dj4@gmail.com>
2021-03-30 18:12:33 +0200

    process.argv en index para llamar a función saludar cuando se pase argumento

---
+++ index.js
@@ -0,0 +1,9 @@
+const saluda = require("./utilidades/archivos.js");
+const chalk = require("chalk");
+
+if (process.argv[2]) {
+  saluda(process.argv[2].replace("nombre=", ""));
+} else {
+  console.log(chalk.yellow("No se ha escrito ningún parámetro."));
+  process.exit(0);
+}
