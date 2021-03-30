c13ba92d43deb0a61c9025366e63878d58ab66b4
Malena de Juan <malena.dj4@gmail.com>
2021-03-30 17:19:00 +0200

    instalado chalk y creada y exportada funcion guardarSaludo

---
+++ utilidades/archivos.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const guardarSaludo = nombre => {
+  fs.writeFile("./textos/saludo.txt", `Hola, ${nombre}. Encantado de saludarte.`, (err) => {
+    if (err) {
+      return console.log(chalk.red.bold("ERROR"));
+    }
+  });
+};
+
+module.exports = guardarSaludo;
